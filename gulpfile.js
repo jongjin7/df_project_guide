929ed0ff6f8894c14e7033d54ee660e8699ae16d
jongjin7 <78jo2918>
2016-10-07 20:11:01 +0900

    패키지, 걸프파일 수정

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,19 @@ var path = require('path');
 
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var minifycss = require('gulp-minify-css');
+var minifyhtml = require('gulp-minify-html');
 var sourcemaps  = require('gulp-sourcemaps');
 var plugins = require('gulp-load-plugins')();
 var runSequence = require('run-sequence');
 var browserSync = require('browser-sync').create();
 var browserify = require('browserify');
-var babelify = require('babelify');
-var watchify = require('watchify');
-var transform = require('vinyl-transform');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
 var plumber = require('gulp-plumber');
-var mergeStream = require('merge-stream');
-var gutil = require('gulp-util');
 var inquirer = require('inquirer');
 
 var pkg = require('./package.json');
-var dirs = pkg['h5bp-configs'].directories;
+var dirs = pkg['configs'].directories;
 
 var errorHandler = function (error) {
     console.error(error.message);
@@ -40,13 +36,7 @@ gulp.task('clean', function (done) {
     ], done);
 });
 
-gulp.task('lint:js', function () {
-    return gulp.src([
-        'gulpfile.js',
-        dirs.devSrc + '/assets/js/**/*.js'
-    ])
-});
-
+//html include
 gulp.task('include',function() {
 
     gulp.src([dirs.devSrc + '/html/**/*.html',
@@ -93,89 +83,28 @@ gulp.task('copy:css', function () {
         .pipe(browserSync.reload({stream:true}));
 });
 
-gulp.task('copy:js0', function () {
+gulp.task('copy:watch-css', function () {
+    return gulp.src([
+            dirs.devSrc + '/assets/css/**/*',
+            '!' + dirs.devSrc + '/assets/css/fonts'
+        ])
+        .pipe(gulp.dest(dirs.buildSrc +'/assets/css'))
+        .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('copy:js', function () {
     return gulp.src([dirs.devSrc + '/assets/js/**/*'])
         .pipe(gulp.dest(dirs.buildSrc +'/assets/js')) //dist 폴더에 저장
         .pipe(browserSync.reload({stream:true})); //browserSync 로 브라우저에 반영
 });
 
-//자바스크립트 파일을 browserify로 번들링
-gulp.task('copy:js', function () {
-    /*var entries = [{
-        src: dirs.devSrc + '/assets/js/common/first1.js',
-        filename: 'first1.js'
-    }, {
-        src: dirs.devSrc + '/assets/js/common/second1.js',
-        filename: 'second1.js'
-    }, {
-        src: dirs.devSrc + '/assets/js/common/third1.js',
-        filename: 'third1.js'
-    }];*/
-
-    var entries =[];
-
-    var $this = this;
-
-    $this.fs    = require("fs");
-    $this.path = dirs.devSrc + '/assets/js/common/';
-
-    //현재 폴더에 존재하는 파일 검색 테스트
-    $this.fs.readdir($this.path, function( err, folders ){
-        for( var i = 0; i < folders.length; i++ ) {
-            var folder = folders[i];
-            var fPath  = $this.path + folder;  // 하위 폴더 경로 반환
-            var files  = $this.fs.readdirSync( fPath );  // 하위 폴더 내 파일 검색
-
-            //console.log('Path:', fPath );
-            for(var h= 0, leng = files.length; h < leng; h++){
-                //console.log( fPath+'/'+files[h] );
-                var tmp = {
-                    src: fPath+'/'+files[h],
-                    filename: files[h]
-                }
-
-                entries.push(tmp)
-
-            }
-
-        };
-    });
-
-    setTimeout(function(){
-        console.log('entries', entries)
-
-        return mergeStream.apply(null, entries.map(function (entry) {
-            var target = watchify(browserify({entries: entry.src, debug: true, cache: {}, packageCache: {}}))
-            //.transform(babelify, {presets: ['interactive', 'common']});
-
-            var bundle = function () {
-                console.log(gutil.colors.green(entry.filename), 'bundling..');
-                console.time(gutil.colors.green(entry.filename));
-
-                return target.bundle()
-                    .on('error', function (err) {
-                        //browserify bundling 과정에서 오류가 날 경우 gulp가 죽지않도록 예외처리
-                        console.error(err);
-                        this.emit('end');
-                    })
-                    .pipe(source('main.js')) //vinyl object 로 변환
-                    .pipe(buffer()) //buffered vinyl object 로 변환
-                    //.pipe(sourcemaps.init({loadMaps: true, debug: true})) //소스맵 생성 준비
-                    //.pipe(uglify()) //minify 해서
-                    //.pipe(sourcemaps.write('./')) //생성된 소스맵을 스트림에 추가
-                    .pipe(gulp.dest(dirs.buildSrc +'/assets/js')) //dist 폴더에 저장
-                    .on('end', function () {
-                        console.timeEnd(gutil.colors.green(entry.filename));
-                    })
-                    .pipe(browserSync.reload({stream: true})); //browserSync 로 브라우저에 반영
-            };
-
-            target.on('update', bundle);
-            return bundle();
-        }));
-    }, 100)
-
-
+gulp.task('copy:watch-js', function () {
+    return gulp.src([
+            dirs.devSrc + '/assets/js/**/*',
+            '!' + dirs.devSrc + '/assets/js/vendor/**/*'
+        ])
+        .pipe(gulp.dest(dirs.buildSrc +'/assets/js'))
+        .pipe(browserSync.reload({stream:true}));
 });
 
 //optimize task
@@ -256,34 +185,14 @@ gulp.task('optimise-project', [
 ]);
 
 
-
-gulp.task('sync', function () {
-    return browserSync.reload({stream:true})
-});
-
-// 파일 변경 감지 및 브라우저 재시작
-gulp.task('watch1', function () {
-    plugins.livereload.listen();
-    gulp.watch(dirs.devSrc + '/**').on('change', plugins.livereload.changed);
-});
-
-gulp.task('webserver',['watch'], function () {
-    var opt = {
-        uri: 'http://localhost:8000/index.html',
-        app:'chrome'
-    }
-    console.log('server start')
-    return gulp.src(dirs.buildSrc + "/")
-        .pipe(plugins.webserver({
-            livereload:true
-        }))
-        .pipe(open(opt))
-});
+// ---------------------------------------------------------------------
+// | Main tasks                                                        |
+// ---------------------------------------------------------------------
 
 //파일 변경 감지 테스트
 gulp.task('watch', function () {
-    gulp.watch(dirs.devSrc + '/assets/js/**/*.js', ['copy:js']);
-    gulp.watch(dirs.devSrc + '/assets/css/**/*.css', ['copy:css']);
+    gulp.watch(dirs.devSrc + '/assets/js/**/*.js', ['copy:watch-js']);
+    gulp.watch(dirs.devSrc + '/assets/css/**/*.css', ['copy:watch-css']);
     gulp.watch(dirs.devSrc + '/html/**/*.html', ['include']);
 });
 
@@ -295,20 +204,7 @@ gulp.task('server', ['copy-project'], function () {
     });
 });
 
-
-
-// ---------------------------------------------------------------------
-// | Main tasks                                                        |
-// ---------------------------------------------------------------------
-
-gulp.task('build0', function (done) {
-    runSequence(
-        ['clean', 'lint:js'],
-        'copy-project',
-        done);
-});
-
-gulp.task('default-s', function (done) {
+gulp.task('sb', function (done) {
     inquirer.prompt([
         {
             type: 'list',
